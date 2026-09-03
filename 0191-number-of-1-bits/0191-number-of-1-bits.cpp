8df86c9390a56440a035831ae76f845054d971c8
Ayush Raj <ayushraj78088@gmail.com>
2026-09-03 10:21:49 +0530

    Time: 0 ms (100%), Space: 8.1 MB (82.52%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -4,8 +4,8 @@ public:
         int cnt = 0;
 
         while (n > 0) {
-            if (n % 2 != 0) cnt++;
-            n /= 2;
+            if  ((n & 1) != 0) cnt++;
+            n = n >> 1;
         }
 
         return cnt;
